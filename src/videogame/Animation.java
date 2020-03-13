f740859156dec7c8142fbdbc026d8d3ad039381a
diegomezcota <gomezcotadiego@gmail.com>
2020-03-12 20:25:29 -0600

    added animation.java documentation

--- src/videogame/Animation.java
+++ src/videogame/Animation.java
@@ -13,18 +13,23 @@ import java.awt.image.BufferedImage;
  */
 public class Animation {
     
-    private int speed;
-    private int index;
-    private long lastTime;
-    private long timer;
-    private BufferedImage[] frames;
+    private int speed;  // for the speed of every frame
+    private int index;  // to get the index of next frame to paint
+    private long lastTime;  // to save the previous time of the animation
+    private long timer; // to acumulate the time of the animation
+    private BufferedImage[] frames; // to store every image - frame
     
+    /**
+     * Creating the animation with all the frames and the speed of each
+     * @param frames an <code>array</code> of images
+     * @param speed an <code>int</code> value for the speed of every frame
+     */
     public Animation(BufferedImage[] frames, int speed) {
-        this.frames = frames;
-        this.speed = speed;
-        index = 0;
-        timer = 0;
-        lastTime = System.currentTimeMillis();
+        this.frames = frames;   // storing frames
+        this.speed = speed;     // storing speed
+        index = 0;              // initializing index
+        timer = 0;              // initializing timer
+        lastTime = System.currentTimeMillis();  // getting the initial time
     }
     
     public BufferedImage getCurrentFrame() {
